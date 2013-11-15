036ea7ce8ea30fd128e55b581aae0d64b7379156
Alejandro Romero <alejo8591@gmail.com>
2013-11-15 12:25:21 -0500

    push initial file for api with endpoints_proto_datastore

---
+++ api/api.py
@@ -0,0 +1,7 @@
+from google.appengine.ext import endpoints
+from google.appengine.ext import ndb
+from protorpc import remote
+
+from endpoints_proto_datastore.ndb import EndpointsMode
+
+from models.Cooperapp import User
