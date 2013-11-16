8c61ce9f807fbf207a744d61b2591bbe3952b92e
Alejandro Romero <alejo8591@gmail.com>
2013-11-16 03:12:54 -0500

    update all structure

---
+++ api/api.py
@@ -0,0 +1,34 @@
+import endpoints
+from google.appengine.ext import ndb
+from protorpc import remote
+from models.Cooperapp import User, Cooperative
+
+@endpoints.api(name='cooperapi', version='v1', description='API for Users Model')
+class CooperApi(remote.Service):	
+	""" Services for User Model """
+	@User.query_method(query_fields=('username',), path='user/{username}', http_method='GET', name='user.get')
+	def UserGet(self, query):
+		return query
+
+	@User.query_method(query_fields=('username', 'email',), path='username', name='user.list')
+	def UserList(self, query):
+		return query
+
+	@User.method(path='user', http_method='POST', name='user.insert')
+	def UserModelInsert(self, user):
+		user.put()
+		return user
+
+	""" Services for Cooperative Model """
+	@Cooperative.method(path='cooperative', http_method='POST', name='cooperative.insert')
+	def CooperativeModelInsert(self, cooperative):
+		cooperative.put()
+		return cooperative
+
+	@Cooperative.query_method(query_fields=('name',), path='cooperative', name='cooperative.list')
+	def CooperativeList(self, query):
+		return query
+
+	@Cooperative.query_method(query_fields=('name',), path='cooperative/{name}', http_method='GET', name='cooperative.get')
+	def CooperativeGet(self, query):
+		return query
---
+++ cooperapp.py
@@ -0,0 +1,25 @@
+import cgi
+# -*- coding: utf-8 -*
+import cgi
+import urllib
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+import endpoints_proto_datastore
+
+import endpoints
+
+import webapp2
+
+from api.api import CooperApi
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('<!DOCTYPE html><html><head><body><h1>Hola mundo</h1></body></html>')
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
+
+endpointsapp = endpoints.api_server([CooperApi], restricted=False)
